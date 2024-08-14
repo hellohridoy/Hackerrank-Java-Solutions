bbb11d73259b035c23524632e328720fd7ebb871
ridoy.hossain <ridoy.hossain@bracits.com>
2024-08-14 23:11:43 +0600

    ifelseproblem

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,27 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+       Scanner input = new Scanner(System.in);
+       int n = input.nextInt();
+       for (int i = 0; i < 1; i++) {
+           if (n % 2 == 1) {
+              System.out.println(" Weird");
+              break;
+           }
+           if (n>3 && n<=5) {
+               System.out.println("Not Weird");
+               continue;
+           }
+           if (n>=6 && n<=20) {
+               System.out.println("Weird");
+           }
+           if (n>20 && n<=100 && n%2==0) {
+               System.out.println("Not Weird");
+           }
+       }
+
+    }
+}
